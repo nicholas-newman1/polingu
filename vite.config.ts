b2a76552a5c3c98822f7313bb50c8e759827c29c
Nicholas Newman <nicholas.newman1@hotmail.com>
2026-02-23 20:29:47 -0500

    add pngs

--- vite.config.ts
+++ vite.config.ts
@@ -48,12 +48,28 @@ export default defineConfig({
         theme_color: '#1976d2',
         background_color: '#ffffff',
         display: 'standalone',
+        start_url: '/',
         icons: [
+          {
+            src: '/polingu-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/polingu-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/polingu-maskable-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
           {
             src: '/polingu.svg',
             sizes: 'any',
             type: 'image/svg+xml',
-            purpose: 'any maskable',
           },
         ],
       },
